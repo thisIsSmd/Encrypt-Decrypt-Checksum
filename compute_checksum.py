816e64289bd912e459dd35236b92aa284fcd5dbf
Mudasir <38310424+thisIsSmd@users.noreply.github.com>
2023-03-18 14:46:20 -0500

    Update compute_checksum.py

--- compute_checksum.py
+++ compute_checksum.py
@@ -9,24 +9,3 @@ codeHash = hashlib.sha256(dataContent).hexdigest()
 
 with open('sensitive_checksum.txt','wb') as f:
     f.write(codeHash.encode())
-
-#5. verify_checksum.py
-
-import hashlib
-
-path = input("Enter the location of the Sensitive file hash\n")
-fileWithHashLoc = input("Enter the location of the file to verify the hash\n")
-
-with open(path, 'rb') as f:
-    OrgFile = f.read().decode()
-    
-
-with open(fileWithHashLoc, 'rb') as f:
-    verifyHashContents = f.read()
-
-hashCode = hashlib.sha256(verifyHashContents).hexdigest()
-
-if ( OrgFile == hashCode ):
-    print("Accept")
-else:
-    print ("Reject")
